87ed92e80afdcb35a3ad807f9abc74369a5950b3
Francis Harbourt <nharbourt@gmail.com>
2023-04-25 16:33:03 -0400

    added client.js but havent tested

---
+++ db/client.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/family';
+
+const client = new Pool({
+    idleTimeoutMillis: 0,
+    connectionString,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  });
+  
+client.on('error', (err, client) => {
+    console.error('Unexpected database error occurred', err);
+  });
+  
+module.exports = client;
+  
